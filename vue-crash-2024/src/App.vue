44cd7434468f4fabb73016167af23d1c2a6037b1
mark <markpackham1@gmail.com>
2024-07-04 11:21:57 +0100

    Add Card component
    https://youtu.be/VeNfHj6MhgA?si=vjWj2lGWQ4gvHF2W&t=4235

--- vue-crash-2024/src/App.vue
+++ vue-crash-2024/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
 import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
   <Navbar />
   <Hero />
+  <HomeCards />
 </template>
--- vue-crash-2024/src/components/HomeCards.vue
+++ vue-crash-2024/src/components/HomeCards.vue
@@ -1,33 +1,36 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from "vue-router";
+import Card from "@/components/Card.vue";
+</script>
 
 <template>
   <section class="py-4">
     <div class="container-xl lg:container m-auto">
       <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-        <div class="bg-gray-100 p-6 rounded-lg shadow-md">
+        <Card>
           <h2 class="text-2xl font-bold">For Developers</h2>
           <p class="mt-2 mb-4">
             Browse our Vue jobs and start your career today
           </p>
-          <a
-            href="jobs.html"
+          <RouterLink
+            to="/jobs"
             class="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
           >
             Browse Jobs
-          </a>
-        </div>
-        <div class="bg-green-100 p-6 rounded-lg shadow-md">
+          </RouterLink>
+        </Card>
+        <Card bg="bg-green-100">
           <h2 class="text-2xl font-bold">For Employers</h2>
           <p class="mt-2 mb-4">
             List your job to find the perfect developer for the role
           </p>
-          <a
-            href="add-job.html"
+          <RouterLink
+            to="/jobs/add"
             class="inline-block bg-green-500 text-white rounded-lg px-4 py-2 hover:bg-green-600"
           >
             Add Job
-          </a>
-        </div>
+          </RouterLink>
+        </Card>
       </div>
     </div>
   </section>

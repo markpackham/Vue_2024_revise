8e1e983a44910d24ffa8bcbe4e0731204ce40a06
mark <markpackham1@gmail.com>
2024-07-04 11:33:45 +0100

    Output jobs from json file
    https://youtu.be/VeNfHj6MhgA?si=3RV1SDbgFEYd-O0x&t=4475

---
+++ vue-crash-2024/src/components/JobListings.vue
@@ -0,0 +1,12 @@
+<script setup>
+import jobsData from "@/jobs1.json";
+import { ref } from "vue";
+
+const jobs = ref(jobsData);
+</script>
+
+<template>
+  <div v-for="job in jobs" :key="job.id">
+    {{ job.title }}
+  </div>
+</template>

40bf51a4f5cf32baae36f3c54ab67784be8b5e69
mark <markpackham1@gmail.com>
2024-07-04 15:50:34 +0100

    Replace Font Awesome with PrimeIcons
    https://youtu.be/VeNfHj6MhgA?si=Gxkn9yMJZiHiTXV2&t=5616

--- vue-crash-2024/src/main.js
+++ vue-crash-2024/src/main.js
@@ -1,6 +1,11 @@
-import './assets/main.css'
+import "./assets/main.css";
+// Replaced Font Awesome with Prime Icons
+// https://github.com/primefaces/primeicons
+// Vue specific version
+// https://primevue.org/icons
+import "primeicons/primeicons.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");

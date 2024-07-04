90dceef83941d83b1349aebc15a4b19d12f96b0e
mark <markpackham1@gmail.com>
2024-07-04 07:31:17 +0100

    Demo v-for
    https://youtu.be/VeNfHj6MhgA?si=G2GoCorBCxczprBF&t=1937

--- vue-crash-2024/src/App.vue
+++ vue-crash-2024/src/App.vue
@@ -3,6 +3,8 @@ export default {
   data() {
     return {
       name: "John Doe",
+      status: "pending",
+      tasks: ["task one", "task two", "task three"],
     };
   },
 };
@@ -10,10 +12,12 @@ export default {
 
 <template>
   <h1>{{ name }}</h1>
-</template>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
 
-<style scoped>
-h1 {
-  color: red;
-}
-</style>
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+</template>

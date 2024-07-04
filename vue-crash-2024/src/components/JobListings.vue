5f2534f48f82c9203adc1bc941a92c613441d4b2
mark <markpackham1@gmail.com>
2024-07-04 12:00:19 +0100

    Add JobListing
    https://youtu.be/VeNfHj6MhgA?si=Gur7denj5LBHKho-&t=4646

--- vue-crash-2024/src/components/JobListings.vue
+++ vue-crash-2024/src/components/JobListings.vue
@@ -11,9 +11,11 @@ const jobs = ref(jobsData);
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse Jobs
       </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div v-for="job in jobs" :key="job.id">
+          {{ job.title }}
+        </div>
+      </div>
     </div>
   </section>
-  <div v-for="job in jobs" :key="job.id">
-    {{ job.title }}
-  </div>
 </template>
